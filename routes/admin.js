238f33cf9a45b1328455712c362d727682ec6f39
ntvinh <btranmakemewonder@gmail.com>
2019-11-05 20:50:13 +0700

    finished

--- routes/admin.js
+++ routes/admin.js
@@ -10,28 +10,35 @@ var router = express.Router();
 // });
 
 router.get('/', function(req, res, next) {
-  res.render('admin/index', { layout: 'layout_admin', action: "Điều khiển" });
+  res.render('admin/admin_login', { layout: false });
 });
 
-router.get('/bieu_do', function(req, res, next) {
-  res.render('admin/bieu_do', { layout: 'layout_admin', action: "Biểu đồ"  });
+router.get('/index', function(req, res, next) {
+  res.render('admin/index', { layout: 'layout_admin', action: "Điều khiển", kind_1: 1 });
+});
+
+router.get('/quan_ly_gian_hang', function(req, res, next) {
+  res.render('admin/quan_ly_gian_hang', { layout: 'layout_admin', action: "Quản lý gian hàng", kind_3: 1 });
 });
 
 router.get('/profile', function(req, res, next) {
-  res.render('admin/profile', { layout: 'layout_admin', action: "Profile"  });
+  res.render('admin/profile', { layout: 'layout_admin', action: "Profile" });
 });
 
 router.get('/quan_ly_account', function(req, res, next) {
-  res.render('admin/quan_ly_account', { layout: 'layout_admin', action: "Quản lý accounts"  });
+  res.render('admin/quan_ly_account', { layout: 'layout_admin', action: "Quản lý accounts", kind_4: 1 });
 });
 
 router.get('/quan_ly_don_hang', function(req, res, next) {
-  res.render('admin/quan_ly_don_hang', { layout: 'layout_admin', action: "Quản lý đơn hàng"  });
+  res.render('admin/quan_ly_don_hang', { layout: 'layout_admin', action: "Quản lý đơn hàng", kind_5: 1 });
 });
 
 router.get('/thong_ke', function(req, res, next) {
-  res.render('admin/thong_ke', { layout: 'layout_admin', action: "Thống kê"  });
+  res.render('admin/thong_ke', { layout: 'layout_admin', action: "Thống kê", kind_2: 1 });
 });
 
+router.get('/chinh_sua_profile', function(req, res, next) {
+  res.render('admin/chinh_sua_profile', { layout: 'layout_admin', action: "Chỉnh sửa profile" });
+});
 
 module.exports = router;
